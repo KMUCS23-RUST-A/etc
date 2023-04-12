f63460040a37c6ac9580595a1978d04fc7006ca7
tassway <juckmyeol0716@gmail.com>
2023-04-12 20:59:37 +0900

    Modify main logic to correct.

--- reaction_speed_test/src/main.rs
+++ reaction_speed_test/src/main.rs
@@ -12,29 +12,33 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("입력을 읽을 수 없습니다.");
 
+        // 사용자가 준비되었는지 확인하는 단계
         if input.trim() != "시작" {
             println!("잘못된 입력입니다.");
             continue;
         }
 
         let delay = Duration::from_secs(rng.gen_range(1..5));
-        println!("{}초 후에 단어가 나타납니다...", delay.as_secs());
         std::thread::sleep(delay);
 
         let word = generate_random_word();
         println!("단어: {}", word);
 
+        // 시작 시각 기록
         let start_time = Instant::now();
         io::stdout().flush().unwrap();
 
         let mut user_input = String::new();
         io::stdin().read_line(&mut user_input).expect("입력을 읽을 수 없습니다.");
-        let end_time = Instant::now();
 
+        // 입력이 맞춰야 하는 단어와 같을 때까지 반복
         if user_input.trim() != word {
             println!("틀렸습니다!");
             continue;
         }
+
+        // 끝난 시각을 기록
+        let end_time = Instant::now();
 
         let round_time = end_time.duration_since(start_time);
         println!("{}초 걸렸습니다!", round_time.as_secs());
